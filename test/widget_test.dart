28c8fcad18bc6b9dc8870c83fa5752c76c256a7a
Motoharu SOMEYA <maretol@outlook.com>
2018-03-25 14:26:03 +0900

    list item controll

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,6 +8,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:myapp/main.dart';
+import 'package:myapp/ui/mainscreen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
