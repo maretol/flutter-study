acd0604b8cc52f9d16be5c3ee2ca1f20a80eb95b
Motoharu SOMEYA <maretol@outlook.com>
2018-03-27 17:13:20 +0900

    list tab and listview controll

--- lib/ui/mainscreen.dart
+++ lib/ui/mainscreen.dart
@@ -1,22 +1,49 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
-import 'dart:developer';
 
 class MyApp extends StatefulWidget{
-
   @override
   _MyAppState createState()=>new _MyAppState();
 }
 
-class _MyAppState extends State<MyApp>{
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin{
 
   _MyAppState(){
-    listItems.add(createListItem("hoge"));
-    listItems.add(createListItem("fuga"));
-    listItems.add(createListItem("piyo"));
+    listItems1.add(createListItem("list1", "hoge"));
+    listItems1.add(createListItem("list1", "fuga"));
+    listItems1.add(createListItem("list1", "piyo"));
+    listItems2.add(createListItem("list2", "hoge"));
+    listItems2.add(createListItem("list2", "hoge"));
+    listItems2.add(createListItem("list2", "fuga"));
+    listItems2.add(createListItem("list2", "piyo"));
+    listItems3.add(createListItem("list3", "hoge"));
+    listItems3.add(createListItem("list3", "fuga"));
+    listItems3.add(createListItem("list3", "piyo"));
+
+    _map = {
+      0: listItems1,
+      1: listItems2,
+      2: listItems3,
+    };
   }
 
-  var listItems = new List<ListTile>();
+  @override
+  void initState(){
+    super.initState();
+    _controller = new TabController(vsync: this, length: 3);
+  }
+
+  @override
+  void dispose(){
+    _controller.dispose();
+    super.dispose();
+  }
+
+  var listItems1 = new List<ListTile>();
+  var listItems2 = new List<ListTile>();
+  var listItems3 = new List<ListTile>();
+  TabController _controller;
+  Map _map;
 
   @override
   Widget build(BuildContext context){
@@ -25,32 +52,45 @@ class _MyAppState extends State<MyApp>{
       home:new Scaffold(
         appBar: new AppBar(
           title: new Text("test"),
+          bottom: new TabBar(
+            controller: _controller,
+            tabs: <Widget>[
+              new Tab(icon: new Icon(Icons.train),),
+              new Tab(icon: new Icon(Icons.directions_car),),
+              new Tab(icon: new Icon(Icons.home),),
+            ],
+          ),
         ),
         floatingActionButton: new FloatingActionButton(
           child: new Icon(Icons.plus_one),
-          onPressed: (){ addListItem("added item"); },
+          onPressed: (){ addListItem(_map[_controller.index], "added item"); },
         ),
-        body: new ListView(
-          children: listItems.toList()
-        ),
+        body:
+          new TabBarView(
+            controller: _controller,
+            children: <Widget>[
+              new ListView(children: listItems1.toList()),
+              new ListView(children: listItems2.toList()),
+              new ListView(children: listItems3.toList())
+            ],
+          )
       )
     );
   }
 
-  ListTile createListItem(String text){
+  ListTile createListItem(String listname,String text){
     return new ListTile(
       title: new Text(
-        text,
+        listname + text,
         style: new TextStyle(fontSize: 24.0),
       ),
       onTap: (){_onTap();},
     );
   }
 
-  void addListItem(text){
+  void addListItem(List<Widget> list ,String text){
     setState((){
-      listItems.insert(0, createListItem("added items"));
-      debugPrint("called\nList items : ${listItems.join(",")}");
+      list.insert(0, createListItem("in list ", " added items"));
     });
   }
 
